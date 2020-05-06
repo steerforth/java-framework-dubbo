3d5e429f3c76be379c4ae420a19d95d8382e0031
steer <565355716@qq,com>
2020-05-06 14:21:13 +0800

    echo 回声测试

---
+++ consumer/src/main/java/com/steer/dubbo/consumer/controller/EchoController.java
@@ -0,0 +1,24 @@
+package com.steer.dubbo.consumer.controller;
+
+import com.steer.dubbo.extservice.service.HelloService;
+import org.apache.dubbo.config.annotation.Reference;
+import org.apache.dubbo.rpc.service.EchoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/echo")
+public class EchoController {
+    @Reference(version = "1.0.0")
+    HelloService helloService;
+    /**
+     * 回声测试，查看服务是否可用
+     * @return
+     */
+    @GetMapping("")
+    public Object echo(){
+        EchoService echoService = (EchoService) helloService;
+        return echoService.$echo("OK");
+    }
+}
